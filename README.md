2d970a1c5afd8d65b9902e03860fabdb40fb303c
Joan Claret <dpam23@gmail.com>
2016-08-24 08:50:22 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,26 @@
+# Boilerplate with React, Webpack & SASS.
+
+- Clone the repo
+- Run `npm i`
+- Run `npm start`
+- Open http://localhost:3333/
+
+
+### Dependencies:
+
+- react
+- react-dom
+
+### Dev dependencies:
+
+- babel
+- babel-core
+- babel-loader
+- babel-preset-es2015
+- babel-preset-react
+- css-loader
+- node-sass
+- sass-loader
+- style-loader
+- webpack
+- webpack-dev-server
---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+module.exports = {
+  entry : './main.js',
+  output: {
+    path: './',
+    filename: 'index.js'
+  },
+  devServer: {
+    inline: true,
+    port: 3333
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['es2015', 'react']
+        }
+      },
+      {
+        test: /\.scss$/,
+        loaders: ['style', 'css', 'sass']
+      }
+    ]
+  }
+}
